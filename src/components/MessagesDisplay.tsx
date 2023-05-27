ff0b11027fa3d6f698271f7cb2e60943e79aeb4c
Gideon Crawley <gscrawley@gmail.com>
2023-05-27 08:45:06 -0700

    made front-end components and styled code display

---
+++ src/components/MessagesDisplay.tsx
@@ -0,0 +1,12 @@
+import MessageDisplay from "./MessageDisplay";
+
+const MessagesDisplay = () => {
+    return (
+      <div className="messages-display">
+        <MessageDisplay />
+      </div>
+    );
+  }
+  
+  export default MessagesDisplay;
+  
