a587ccf76365f755a8cec4212c0c8819003f60e8
Gideon Crawley <gscrawley@gmail.com>
2023-05-27 16:53:07 -0700

    backend ready

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,36 @@ import MessagesDisplay from "./components/MessagesDisplay";
 
 
 const App = () => {
+  const getQuery = async () => {
+    try {
+      const options = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ 
+          message: "create a table"
+      })
+    }
+    
+  const response = await fetch("http://localhost:8000/completions", options)
+      const data = await response.json()
+      console.log(data)
+    } catch (error) {
+      console.log(error)
+    }
   return (
     <div className="app">
       <MessagesDisplay />
       <input/>
       <CodeDisplay />
       <div className="button-container">
-        <button id="get-query">Get Query!</button>
+        <button id="get-query" onClick={getQuery}>Get Query!</button>
         <button id="clear-chat">Clear Chat</button>
       </div>
     </div>
   );
 }
+}
 
-export default App;
+export default App
